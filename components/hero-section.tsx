8afdf1e2a9348b74917bac316ddb7a3261425abd
gooding Solutions <andrepsss@gmail.com>
2025-03-28 14:04:31 -0300

    text update

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -38,7 +38,7 @@ export default function HeroSection() {
           <motion.div variants={itemVariants}>
             <Image src="/images/logo-porto-siao.svg" alt="Logo Montesiao" width={150} height={150} className="mb-4" />
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4 leading-tight text-left">
-              A melhor forma de comprar o seu Imóvel é com o Consórcio!
+              Aproveite parcelas 40% menores no Consórcio Porto Sião!
             </h1>
           </motion.div>
 
